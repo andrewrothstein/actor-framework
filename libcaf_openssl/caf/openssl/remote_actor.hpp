c3d1167f634601d9d821e88a4cd6e4cc55ad3374
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2017-03-22 10:48:54 +0100

    Add scaffold for OpenSSL module

---
+++ libcaf_openssl/caf/openssl/remote_actor.hpp
@@ -0,0 +1,47 @@
+/******************************************************************************
+ *                       ____    _    _____                                   *
+ *                      / ___|  / \  |  ___|    C++                           *
+ *                     | |     / _ \ | |_       Actor                         *
+ *                     | |___ / ___ \|  _|      Framework                     *
+ *                      \____/_/   \_|_|                                      *
+ *                                                                            *
+ * Copyright (C) 2011 - 2017                                                  *
+ * Dominik Charousset <dominik.charousset (at) haw-hamburg.de>                *
+ *                                                                            *
+ * Distributed under the terms and conditions of the BSD 3-Clause License or  *
+ * (at your option) under the terms and conditions of the Boost Software      *
+ * License 1.0. See accompanying files LICENSE and LICENSE_ALTERNATIVE.       *
+ *                                                                            *
+ * If you did not receive a copy of the license files, see                    *
+ * http://opensource.org/licenses/BSD-3-Clause and                            *
+ * http://www.boost.org/LICENSE_1_0.txt.                                      *
+ ******************************************************************************/
+
+#include <set>
+#include <string>
+#include <cstdint>
+
+#include "caf/fwd.hpp"
+
+namespace caf {
+namespace openssl {
+
+/// Establish a new connection to the actor at `host` on given `port`.
+/// @param host Valid hostname or IP address.
+/// @param port TCP port.
+/// @returns An `actor` to the proxy instance representing
+///          a remote actor or an `error`.
+template <class ActorHandle = actor>
+expected<ActorHandle> remote_actor(actor_system& sys, std::string host,
+                                   uint16_t port) {
+  detail::type_list<ActorHandle> tk;
+  auto x = remote_actor(system().message_types(tk), std::move(host), port);
+  if (!x)
+    return x.error();
+  CAF_ASSERT(x && *x);
+  return actor_cast<ActorHandle>(std::move(*x));
+}
+
+} // namespace openssl
+} // namespace caf
+
